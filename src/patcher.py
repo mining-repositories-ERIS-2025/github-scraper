20e1b16a1925e12d2769698febcadd809d35e3b6
EmilSpangenberg <emspa15@student.sdu.dk>
2025-04-16 16:36:40 +0200

    added ast parsing to patcher for True / False

--- src/patcher.py
+++ src/patcher.py
@@ -2,6 +2,8 @@ from models.bcolor import bcolors
 from io import BytesIO
 from collections import Counter
 import tokenize
+import re
+import ast
 
 class Patcher:
     def patch_file(self, file: dict) -> dict:
@@ -30,21 +32,34 @@ class Patcher:
         
         return patched_file
 
-    def tokenize_lines(self, lines):
+    def tokenize_lines(self, lines, deleted_lines=None):
         token_counts = Counter()
-        for _, line in lines:
-            tokens = self.extract_tokens(line)
+        
+        for idx, (i, line) in enumerate(lines):
+            deleted_line = deleted_lines[idx][1] if deleted_lines and idx < len(deleted_lines) else None
+            tokens = self.extract_tokens(line, deleted_line)
             token_counts.update(tokens)
 
         return dict(sorted(token_counts.items(), key=lambda item: item[1], reverse=True))
 
-    def extract_tokens(self, code):
+
+    def extract_tokens(self, code, deleted_line=None):
         tokens = []
         try:
             cleaned_code = code.strip()
             if not cleaned_code:
                 return tokens
-            
+
+            try:
+                parsed = ast.parse(cleaned_code)
+                for node in ast.walk(parsed):
+                    if isinstance(node, ast.Call):
+                        for kw in node.keywords:
+                            if isinstance(kw.value, ast.Constant) and kw.value.value in {True, False}:
+                                tokens.append(f"{kw.arg}={kw.value.value}")
+            except SyntaxError:
+                pass
+
             fake_file = BytesIO(f'\n{cleaned_code}'.encode('utf-8'))
             token_generator = tokenize.tokenize(fake_file.readline)
             for token in token_generator:
@@ -56,3 +71,4 @@ class Patcher:
             print(bcolors.FAIL + str(e) + bcolors.ENDC)
 
         return tokens
+
